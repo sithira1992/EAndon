a679a7192602e7c650830ded060c19c5e2587f15
sithira1992 <sithira2012@gmail.com>
2015-07-23 16:53:51 +0530

    uopdte

--- scripts/application/controllers/admin/controller.js
+++ scripts/application/controllers/admin/controller.js
@@ -7,6 +7,7 @@
             function ($scope, $http,$routeParams,$filter,$location) {
 
             $scope.pagedItems = [];
+                $scope.messege='lol';
 
             $scope.get_staff = function () {
                 $http.get('db/staffRegistration.php?action=get_staff').success(function (data) {
@@ -25,17 +26,43 @@
                     'nic': $scope.nic, 'phone': $scope.phone, 'position': $scope.position, 'email': $scope.email
                 }).success(function (data, status, headers, config) {
                     if (data.msg != '') {
+
+                        $scope.messege='Staff Member Registration Successfully';
+                        $scope.notify('success');
                         $scope.msgs.push(data.msg);
+
                     }
                     else {
+
+                        $scope.messege='Staff Member Registration Not Successfully';
+                        $scope.notify('error');
                         $scope.msgs.push(data.error);
                     }
                 }).error(function (data, status) { // called asynchronously if an error occurs
 // or server returns response with an error status.
-                    $scope.msgs.push(status);
 
+                    $scope.messege='Staff Member Registration Not Successfully';
+                    $scope.notify('error');
                 });
             }
 
-            }]);
+
+                $scope.notify=function(style)
+            {
+
+                 $.notify({
+             title: 'Submission Status',
+             text: $scope.messege,
+             image: "<img src='./images/unsuccess.png'/>"
+          }, {
+                      style: 'metro',
+                      className: style,
+                  autoHide: true,
+                   clickToHide: true
+               });
+}
+            }])
+
+
+    ;
 })(angular);

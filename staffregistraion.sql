9e63bf4987a686a70ce3cad4b879987bc9abbc37
sithira1992 <sithira2012@gmail.com>
2015-07-27 20:57:08 +0530

    sql staff

--- staffregistraion.sql
+++ staffregistraion.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2015 at 06:28 PM
+-- Generation Time: Jul 27, 2015 at 05:26 PM
 -- Server version: 5.5.34
 -- PHP Version: 5.4.22
 
@@ -35,35 +35,33 @@ CREATE TABLE IF NOT EXISTS `staffregistraion` (
   `phone` varchar(45) DEFAULT NULL,
   `jobPosition` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
-  `regsterDate` datetime DEFAULT NULL,
+  `registerDate` datetime DEFAULT NULL,
   `status` varchar(45) DEFAULT NULL,
-  `rr` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
 
 --
 -- Dumping data for table `staffregistraion`
 --
 
-INSERT INTO `staffregistraion` (`id`, `fullName`, `address`, `gender`, `nic`, `phone`, `jobPosition`, `email`, `regsterDate`, `status`, `rr`) VALUES
-(1, 'Mahesh Udara', 'Galle', 'male', '920456896V', '0758596954', NULL, NULL, NULL, NULL, NULL),
-(7, 'Sithira Pramudith', 'Galle', 'male', '920547856V', '0715896568', NULL, NULL, NULL, NULL, NULL),
-(8, 'Kanchana Silva', 'Galle', 'male', '458758956V', '0715478582', NULL, NULL, NULL, NULL, NULL),
-(9, 'Duminda Rathnayaka', 'Yakkalamulla', 'male', '58578488dV', '0715478582', NULL, NULL, NULL, NULL, NULL),
-(10, 'Presanna baddewithana', 'Wakwella', 'male', '45854858V', '078547852', NULL, NULL, NULL, NULL, NULL),
-(11, 'Mahasen Bandara', 'Dewata', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, NULL, NULL),
-(12, 'Gayashan Alwis', 'Ginthota', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, NULL, NULL),
-(13, 'Mawan Senanayaka', 'Hikkaduwa', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, NULL, NULL),
-(14, 'Malan jagath', 'Ambalangoda', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, NULL, NULL),
-(15, 'Saman Kumara', 'Akuressa', 'male', '920915210v', '0716279549', 'saab', 'a@gmail.com', NULL, NULL, NULL),
-(16, 'Nadun Samarasinha', 'Imaduwa', 'male', '920915210v', '0716279549', 'saab', 'a@gmail.com', NULL, NULL, NULL),
-(17, 'MSamantha Gunawardna', 'Pilana', 'male', '920915211v', '0716279549', 'opel', 'a@gmail.com', NULL, NULL, NULL),
-(18, 'Kasun gayanga', 'Opatha', 'male', '920460348v', '0716362464', 'saab', 'mucreationz@gmail.com', NULL, NULL, NULL),
-(19, 'Kasun Gunathilaka', 'Udugama', '', '', '', '', '', NULL, NULL, NULL),
-(20, 'Kasun Sandeep', 'Wanchawala', 'male', '920460258V', '0716362464', 'volvo', 'mj@gmail.com', NULL, NULL, NULL),
-(21, 'Naween Disanayaka', 'Bogahagoda', 'male', '920560458V', '0715263654', 'opel', 'muy@gmail.com', NULL, NULL, NULL),
-(22, 'Mewan Jayasinha', 'Imaduwa', 'male', '920460345V', '0913920254', 'saab', 'mk@gmail.com', NULL, NULL, NULL),
-(23, 'Dinelka Randunu', 'Imaduwa', 'male', '920460345V', '0913920254', 'Site Manager', 'mk@gmail.com', NULL, NULL, NULL);
+INSERT INTO `staffregistraion` (`id`, `fullName`, `address`, `gender`, `nic`, `phone`, `jobPosition`, `email`, `registerDate`, `status`) VALUES
+(1, 'aa', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+(7, 'q', 'q', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+(8, 'q', 'q', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
+(9, 'a', 's', 'male', 'a', NULL, NULL, NULL, NULL, '1'),
+(10, 'a', 's', 'male', 'a', NULL, NULL, NULL, NULL, '1'),
+(11, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '1'),
+(12, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '1'),
+(13, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '0'),
+(14, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '0'),
+(15, 'sithira', 'Galle', 'male', '920915210v', '0716279549', 'saab', 'a@gmail.com', NULL, '1'),
+(16, 'sithira', 'Galle', 'male', '920915210v', '0716279549', 'QS', 'a@gmail.com', NULL, '1'),
+(17, 'sithira', 'aa', 'male', '920915211v', '0716279549', 'Site Manager', 'a@gmail.com', NULL, '1'),
+(18, 'aa', 'a', 'male', '92091', '0716279549', 'volvo', 'a', NULL, '0'),
+(19, 'a', 'a', 'male', '920915218v', '0716279549', 'audi', 'a@gmail.com', NULL, '0'),
+(20, 'a', 'a', 'male', '920915213v', '0716279549', 'saab', 'a@gmail.com', NULL, '0'),
+(21, 'sithira', 'Galle', 'male', '900910528v', '0716279549', 'Admin', 'a@gmail.com', NULL, '0'),
+(22, 'sithira Pramudith', 'Galle\nImaduwa\naaa', 'male', '92091528v', '0716279549', 'Admin', 'sit@gmail.com', '2015-07-25 00:00:00', '1');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

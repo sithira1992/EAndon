4ceb1f874d4b715a18bf6e17a7eecda0e6a912e5
sithira1992 <sithira2012@gmail.com>
2015-08-02 21:56:18 +0530

    db

--- staffregistraion.sql
+++ staffregistraion.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 27, 2015 at 05:26 PM
+-- Generation Time: Aug 02, 2015 at 06:06 PM
 -- Server version: 5.5.34
 -- PHP Version: 5.4.22
 
@@ -37,6 +37,7 @@ CREATE TABLE IF NOT EXISTS `staffregistraion` (
   `email` varchar(45) DEFAULT NULL,
   `registerDate` datetime DEFAULT NULL,
   `status` varchar(45) DEFAULT NULL,
+  `password` varchar(450) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
 
@@ -44,24 +45,24 @@ CREATE TABLE IF NOT EXISTS `staffregistraion` (
 -- Dumping data for table `staffregistraion`
 --
 
-INSERT INTO `staffregistraion` (`id`, `fullName`, `address`, `gender`, `nic`, `phone`, `jobPosition`, `email`, `registerDate`, `status`) VALUES
-(1, 'aa', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0'),
-(7, 'q', 'q', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
-(8, 'q', 'q', NULL, NULL, NULL, NULL, NULL, NULL, '0'),
-(9, 'a', 's', 'male', 'a', NULL, NULL, NULL, NULL, '1'),
-(10, 'a', 's', 'male', 'a', NULL, NULL, NULL, NULL, '1'),
-(11, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '1'),
-(12, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '1'),
-(13, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '0'),
-(14, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '0'),
-(15, 'sithira', 'Galle', 'male', '920915210v', '0716279549', 'saab', 'a@gmail.com', NULL, '1'),
-(16, 'sithira', 'Galle', 'male', '920915210v', '0716279549', 'QS', 'a@gmail.com', NULL, '1'),
-(17, 'sithira', 'aa', 'male', '920915211v', '0716279549', 'Site Manager', 'a@gmail.com', NULL, '1'),
-(18, 'aa', 'a', 'male', '92091', '0716279549', 'volvo', 'a', NULL, '0'),
-(19, 'a', 'a', 'male', '920915218v', '0716279549', 'audi', 'a@gmail.com', NULL, '0'),
-(20, 'a', 'a', 'male', '920915213v', '0716279549', 'saab', 'a@gmail.com', NULL, '0'),
-(21, 'sithira', 'Galle', 'male', '900910528v', '0716279549', 'Admin', 'a@gmail.com', NULL, '0'),
-(22, 'sithira Pramudith', 'Galle\nImaduwa\naaa', 'male', '92091528v', '0716279549', 'Admin', 'sit@gmail.com', '2015-07-25 00:00:00', '1');
+INSERT INTO `staffregistraion` (`id`, `fullName`, `address`, `gender`, `nic`, `phone`, `jobPosition`, `email`, `registerDate`, `status`, `password`) VALUES
+(1, 'aa', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL),
+(7, 'q', 'q', NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL),
+(8, 'q', 'q', NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL),
+(9, 'a', 's', 'male', 'a', NULL, NULL, NULL, NULL, '0', NULL),
+(10, 'a', 's', 'male', 'a', NULL, NULL, NULL, NULL, '1', NULL),
+(11, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '1', NULL),
+(12, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '1', NULL),
+(13, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '0', NULL),
+(14, 'a', 'a', 'male', '920915210v', '0716279549', 'saab', NULL, NULL, '0', NULL),
+(15, 'sithira', 'Galle', 'male', '920915210v', '0716279549', 'saab', 'a@gmail.com', NULL, '1', NULL),
+(16, 'sithira', 'Galle', 'male', '920915210v', '0716279549', 'QS', 'a@gmail.com', NULL, '1', NULL),
+(17, 'sithira', 'aa', 'male', '920915211v', '0716279549', 'Site Manager', 'a@gmail.com', NULL, '1', NULL),
+(18, 'aa', 'a', 'male', '92091', '0716279549', 'volvo', 'a', NULL, '1', NULL),
+(19, 'a', 'a', 'male', '920915218v', '0716279549', 'audi', 'a@gmail.com', '0000-00-00 00:00:00', '1', NULL),
+(20, 'a', 'a', 'male', '920915213v', '0716279549', 'saab', 'a@gmail.com', NULL, '1', NULL),
+(21, 'sithira Pramudith', 'Galle', 'male', '900910528v', '0716279549', 'Admin', 'a@gmail.com', NULL, '1', 'a'),
+(22, 'sithira Pramudith', 'Galle\nImaduwa', 'male', '92091528v', '0716279549', 'Admin', 'sith@gmail.com', '2015-07-25 00:00:00', '1', 'aaa');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
